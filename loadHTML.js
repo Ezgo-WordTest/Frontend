8dbcdc255a44ca3e2ffef7f3aebb52e249db7f89
U-P2542\JIM <JIM@P2542.(none)>
2014-11-28 17:14:13 +0800

    Add layout and sample

---
+++ loadHTML.js
@@ -0,0 +1,8 @@
+// Define function and class here
+function load()
+{
+	document.getElementById("index").innerHTML='<object type="text/html" width="1024" height="1080" data="sample.html" ></object>';
+}
+
+//implement here			
+load();
