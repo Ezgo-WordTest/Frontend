6d4bed3bbe2aa557e758cc5a9ea9dcd6812505d6
jackraken <apple894894@gmail.com>
2014-11-28 10:55:46 +0800

    modified option.html, add two pictures
    add navigator click action,add background picture, change button text

--- app.js
+++ app.js
@@ -1,5 +1,85 @@
 var main = function() {
   
+  $('.list1').click(function() {
+    var currentSlide = $('.active_slide');
+    var nextSlide = $('.slide1');
+
+    var currentDot = $('.active-dot');
+    var nextDot = $('.dot1');
+
+    var currentNav = $('.active');
+    var nextNav = $('.list1');
+
+    currentSlide.removeClass('active_slide');
+    nextSlide.addClass('active_slide');
+
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+
+    currentNav.removeClass('active');
+    nextNav.addClass('active');
+  });
+
+  $('.list2').click(function() {
+    var currentSlide = $('.active_slide');
+    var nextSlide = $('.slide2');
+
+    var currentDot = $('.active-dot');
+    var nextDot = $('.dot2');
+
+    var currentNav = $('.active');
+    var nextNav = $('.list2');
+
+    currentSlide.removeClass('active_slide');
+    nextSlide.addClass('active_slide');
+
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+
+    currentNav.removeClass('active');
+    nextNav.addClass('active');
+  });
+
+  $('.list3').click(function() {
+    var currentSlide = $('.active_slide');
+    var nextSlide = $('.slide3');
+
+    var currentDot = $('.active-dot');
+    var nextDot = $('.dot3');
+
+    var currentNav = $('.active');
+    var nextNav = $('.list3');
+
+    currentSlide.removeClass('active_slide');
+    nextSlide.addClass('active_slide');
+
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+
+    currentNav.removeClass('active');
+    nextNav.addClass('active');
+  });
+
+  $('.list4').click(function() {
+    var currentSlide = $('.active_slide');
+    var nextSlide = $('.slide4');
+
+    var currentDot = $('.active-dot');
+    var nextDot = $('.dot4');
+
+    var currentNav = $('.active');
+    var nextNav = $('.list4');
+
+    currentSlide.removeClass('active_slide');
+    nextSlide.addClass('active_slide');
+
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+
+    currentNav.removeClass('active');
+    nextNav.addClass('active');
+  });
+
   $('.arrow-next').click(function() {
     var currentSlide = $('.active_slide');
     var nextSlide = currentSlide.next();
@@ -7,16 +87,23 @@ var main = function() {
     var currentDot = $('.active-dot');
     var nextDot = currentDot.next();
 
+    var currentNav = $('.active');
+    var nextNav = currentNav.next();
+
     if(nextSlide.length === 0) {
       nextSlide = $('.slide').first();
       nextDot = $('.dot').first();
+      nextNav = $('.mynav').first();
     }
     
-    currentSlide.fadeOut(600).removeClass('active_slide');
-    nextSlide.fadeIn(600).addClass('active_slide');
+    currentSlide.removeClass('active_slide');
+    nextSlide.addClass('active_slide');
 
     currentDot.removeClass('active-dot');
     nextDot.addClass('active-dot');
+
+    currentNav.removeClass('active');
+    nextNav.addClass('active');
   });
 
 
@@ -27,16 +114,23 @@ var main = function() {
     var currentDot = $('.active-dot');
     var prevDot = currentDot.prev();
 
+    var currentNav = $('.active');
+    var nextNav = currentNav.prev();
+
     if(prevSlide.length === 0) {
       prevSlide = $('.slide').last();
       prevDot = $('.dot').last();
+      nextNav = $('.mynav').last();
     }
     
-    currentSlide.fadeOut(600).removeClass('active_slide');
-    prevSlide.fadeIn(600).addClass('active_slide');
+    currentSlide.removeClass('active_slide');
+    prevSlide.addClass('active_slide');
 
     currentDot.removeClass('active-dot');
     prevDot.addClass('active-dot');
+
+    currentNav.removeClass('active');
+    nextNav.addClass('active');
   });
 
 }
