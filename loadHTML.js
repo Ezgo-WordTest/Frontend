791fa830622e45f2003e0122e3cea3497adabb13
Eddy0402 <yukkcake@gmail.com>
2014-12-01 15:36:17 +0800

    Remove unneccersary file.

--- loadHTML.js
+++
@@ -1,8 +0,0 @@
-// Define function and class here
-function load()
-{
-	document.getElementById("index").innerHTML='<object type="text/html" width="1024" height="1080" data="sample.html" ></object>';
-}
-
-//implement here
-load();
