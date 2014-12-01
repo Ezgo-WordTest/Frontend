745ca74764abb6e5052648fddaa7a25106725808
Eddy0402 <yukkcake@gmail.com>
2014-12-01 13:18:37 +0800

    Cleanup indent sytle, remove unneccersary file.

--- loadHTML.js
+++ loadHTML.js
@@ -4,5 +4,5 @@ function load()
 	document.getElementById("index").innerHTML='<object type="text/html" width="1024" height="1080" data="sample.html" ></object>';
 }
 
-//implement here			
+//implement here
 load();
