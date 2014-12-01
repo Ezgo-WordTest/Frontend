5f0262176574f72121d4d41d966249cd66e85c32
Eddy0402 <yukkcake@gmail.com>
2014-12-01 16:00:07 +0800

    Make CSS files load dynamically

--- app.js
+++ app.js
@@ -138,4 +138,17 @@ var main = function() {
   });
 }
 
+function loadCSS(file){
+    if($('link[href="' + file + '"]').size() > 0){ 
+        console.log('already loaded ' + file);
+    }else{
+      if (document.createStyleSheet){
+        document.createStyleSheet( file );
+      }
+      else {
+        $("head").append($('<link rel="stylesheet" href="' + file +'" type="text/css" />'));
+      }
+    }
+}
+
 $(document).ready(main);
