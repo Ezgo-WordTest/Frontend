9c59d6b352e17b6111c0206b6735a66ef80c3083
Eddy0402 <yukkcake@gmail.com>
2014-12-05 12:21:07 +0800

    quest.js: move `layout_init` after questions setup, add question filter.
    Add a question filter template to select question, currently no effect
    to the array.

--- js/quest.js
+++ js/quest.js
@@ -7,26 +7,33 @@ var option_setting;
 var questions;
 var current_question;
 
-var Question = function(topic,description,option1,option2,option3,option4){
+var Question = function(topic, description, option1, option2, option3, option4){
   this.topic = topic;
   this.description = description;
   this.option = { 1:option1, 2:option2, 3:option3, 4:option4 };
 };
 
-var question_init = function(){
+function question_init(){
 
   $.getJSON("all_questions.json", function(data) {
-    questions = data;
-    current_question = new Question(questions[0].question,questions[0].ps,questions[0].answer,questions[0].option1,questions[0].option2,questions[0].option3);
-  });
-  alert(option_setting[0]);
-  alert(option_setting[1]);
-  alert(option_setting[2]);
-  alert(option_setting[3]);
 
-  //current_question = new Question('Hello','Test Description', 'opt1','opt2','opt3','opt4');
-  //current_question = new Question(questions[0].question,questions[0].ps,questions[0].answer,questions[0].option1,questions[0].option2,questions[0].option3);
-  layout_init();
+    console.log("Question count:" + data.length);
+    var questions = question_filter(data, option_setting);
+    console.log("Question count:" + questions.length);
+
+    current_question = new Question(questions[0].question,questions[0].ps,questions[0].answer,questions[0].option1,questions[0].option2,questions[0].option3);
+
+    layout_init();
+  });
+  console.log(option_setting[0]);
+  console.log(option_setting[1]);
+  console.log(option_setting[2]);
+  console.log(option_setting[3]);
+}
+
+function question_filter(raw_data, option_setting){
+    // Select questions
+    return raw_data;
 }
 
 var layout_init = function(){
