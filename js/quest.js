20cec5581d057caecc8ba4759c46f8d1e953a784
jackraken <apple894894@gmail.com>
2014-12-05 15:52:44 +0800

    quest: read ramdom question, add question switch

--- js/quest.js
+++ js/quest.js
@@ -7,6 +7,9 @@ var option_setting;
 var questions;
 var current_question;
 
+var selected_questions=[];
+var current_no=0;
+
 var Question = function(topic,description,option1,option2,option3,option4){
   this.topic = topic;
   this.description = description;
@@ -14,15 +17,15 @@ var Question = function(topic,description,option1,option2,option3,option4){
 };
 
 var question_init = function(){
-
+  selected_num = 1;
   $.getJSON("all_questions.json", function(data) {
     questions = data;
     current_question = new Question(questions[0].question,questions[0].ps,questions[0].answer,questions[0].option1,questions[0].option2,questions[0].option3);
   });
-  alert(option_setting[0]);
-  alert(option_setting[1]);
-  alert(option_setting[2]);
-  alert(option_setting[3]);
+  //alert(option_setting[0]);
+  //alert(option_setting[1]);
+  //alert(option_setting[2]);
+  //alert(option_setting[3]);
 
   //current_question = new Question('Hello','Test Description', 'opt1','opt2','opt3','opt4');
   //current_question = new Question(questions[0].question,questions[0].ps,questions[0].answer,questions[0].option1,questions[0].option2,questions[0].option3);
@@ -53,3 +56,41 @@ var question_display = function(){
 
 $(document).ready(question_init);
 
+var next_quest = function(){
+  current_no++;
+  //alert(selected_questions.length);
+  if(current_no >= option_setting[1]){
+    alert('WQW');
+    current_no = 0;
+    current_question = selected_questions[0];
+  }
+  else if(selected_questions.length < option_setting[1]){
+    alert('WOW');
+    var random_number=Math.floor(Math.random()*1001);
+    $.getJSON("all_questions.json", function(data) {
+      questions = data;
+      current_question = new Question(questions[random_number].question,questions[random_number].ps,questions[random_number].answer,questions[random_number].option1,questions[random_number].option2,questions[random_number].option3);
+      selected_questions[current_no]=current_question;
+    });
+  }
+  else{
+    current_question = selected_questions[current_no];
+  }
+  $("#question-placeholder").load("quest-" + question_type[q_setting] + ".html", question_display );
+}
+
+var prev_quest = function(){
+  current_no--;
+  var random_number=Math.floor(Math.random()*1001);
+  current_question = selected_questions[current_no];
+  $("#question-placeholder").load("quest-" + question_type[q_setting] + ".html", question_display );
+}
+
+$('.glyphicon-chevron-left').click(function() {
+  prev_quest();
+});
+  
+$('.glyphicon-chevron-right').click(function() {
+  next_quest();
+});
+  
